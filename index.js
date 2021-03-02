33b8ba4fc7f24676924833c1ad499779fe3fb371
Marcus Pessoa <marcusvictor00@gmail.com>
2021-03-02 12:35:01 -0300

    Inclusão da rota resultado

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+// importando express e engine de templates
 const express = require('express')
-const path = require('path')
-const servidor = express()
+const pug = require('pug')
+
+// definindo app e porta
+const app = express()
 const porta = 8023
 
-//rota e função de callback que retorna 
-servidor.get('/', (req, res) => {
-    return res.sendFile(path.join(__dirname + '/index.html'))
+// configuração da engine de templates
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
+//rota que retorna a pagina index
+app.get('/', (req, res) => {
+    return res.render('index')
 })
 
-//Executa o servidor
-servidor.listen(porta, () => {
+app.post('resultado', (req, res) => {
+    return res.render('resultado')
+})
+
+//Executa o app
+app.listen(porta, () => {
     console.log(`Aplicação rodando em http://localhost:${porta}`)
 })
