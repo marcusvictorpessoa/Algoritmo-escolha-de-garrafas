e1ef8fd0d94ffdc86549a137642c017315b5309e
Marcus Pessoa <marcusvictor00@gmail.com>
2021-03-03 22:59:48 -0300

    Arquivo principal parcialmente finalizado, restando apenas inclusão do algoritimo para selecionas as garrafas.

--- index.js
+++ index.js
@@ -15,15 +15,16 @@ app.use(express.urlencoded({extended: false}))
 
 // rota que retorna a pagina index
 app.get('/', (req, res) => {
-    return res.render('index')
+    const titulo_pagina = "Página inicial"
+    return res.render('index',{titulo_pagina})
 })
 
 // rota que retorna pagina de resultado
 app.post('/resultado', (req, res) => {
-    //console.log(req.body)
+    const titulo_pagina = "Resultado"
     const { galao, garrafas } = req.body
-    console.log(`Galão: ${galao} Garrafas ${garrafas}`)
-    return res.render('resultado')
+    let sobra = 0
+    return res.render('resultado', { titulo_pagina, galao, garrafas, sobra })
 })
 
 // Execução do app
